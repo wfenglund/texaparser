fc9aa77b4cc9f72b1dca3d6579b7951b39687e84
William Englund <w.f.englund@gmail.com>
2024-09-29 02:52:38 +0200

    fixed so that sgt walker prints moves correctly

--- sgt_walker.py
+++ sgt_walker.py
@@ -19,8 +19,8 @@ while player_hand != 'q':
         pad_mve = [i.strip().ljust(pad + 1, ' ') for i in opt_moves]
         pad_pos = [i.strip().ljust(pad + 1, ' ') for i in pos_names]
         print(f'{player_hand}:')
-        print(f'{''.join(pad_pos)}\n{''.join(pad_mve)}\n')
-        print(f'{''.join(pad_pos)}\n{''.join(pad_mve)}\n')
-        print(f'{''.join(pad_pos)}\n{''.join(pad_mve)}\n')
-        print(f'{''.join(pad_pos)}\n{''.join(pad_mve)}\n')
+        print(f'{''.join(pad_pos[:8])}\n{''.join(pad_mve[:8])}\n')
+        print(f'{''.join(pad_pos[9:17])}\n{''.join(pad_mve[9:17])}\n')
+        print(f'{''.join(pad_pos[18:26])}\n{''.join(pad_mve[18:26])}\n')
+        print(f'{''.join(pad_pos[27:35])}\n{''.join(pad_mve[27:35])}\n')
     player_hand = input('Hand> ')
